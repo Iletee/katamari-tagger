58e8cb77b46ce8c2eb1f214813aa14d6d7585309
Ilkka Turunen <ilkka@nestronite.com>
2012-06-03 23:06:48 +0300

    slight test

--- katamari.js
+++ katamari.js
@@ -2,38 +2,39 @@
   // This is hosted off of github
   // because we don't want to deal with setting up and serving off of SSL
   // through our domain provider
-  window.__wlw = {};
-  window.__wlw.loading_jQuery = false;
-  window.__wlw.loader = function() {
+  window.__kt = {};
+  window.__kt.loading_jQuery = false;
+  window.__kt.loader = function() {
      console.log('loading');
     if (typeof(jQuery) === 'undefined') {
-      if (window.__wlw.loading_jQuery) {
-        setTimeout(window.__wlw.loader, 100);
+      if (window.__kt.loading_jQuery) {
+        setTimeout(window.__kt.loader, 100);
       } else {
-        window.__wlw.loading_jQuery= true;
+        window.__kt.loading_jQuery= true;
         var jq_script = document.createElement('script');
         jq_script.src = '//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
         document.body.appendChild(jq_script);
         
 		//go to next function
-		window.__wlw.loader();
+		window.__kt.loader();
       }
     } else {
-	//	alert('JQuery present!');
+		console.err('JQuery present!');
     }
 	console.log('loading done');
   }
 
-  window.__wlw.live = function(jQuery) {
+  window.__kt.live = function(jQuery) {
     
   };
 
-  window.__wlw.getTextNodesIn = function(jQuery, el) {
+  window.__kt.getTextNodesIn = function(jQuery, el) {
     
   };
 
-  window.__wlw.hideWords = function(jQuery, include_transitions) {
+  window.__kt.hideWords = function(jQuery, include_transitions) {
    
   }
-  window.__wlw.loader();
+  //Load katamari
+  window.__kt.loader();
 })();
