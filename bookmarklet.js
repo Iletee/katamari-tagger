83376bf035369cc3c72f2124becd01f593d1a145
Ilkka Turunen <ilkka@nestronite.com>
2012-06-03 22:42:37 +0300

    Added bookmarklet and fixed typos
    Added browser bookmark bar bookmarklet code that loads katamari.js to any
    page. Will work as basis for future work.

---
+++ bookmarklet.js
@@ -0,0 +1,2 @@
+//Add this javascript to a bookmark bar
+javascript:void(function(){var jsCode=document.createElement('script');jsCode.setAttribute('src','https://raw.github.com/Ile2/katamari-tagger/master/katamari.js');document.body.appendChild(jsCode);}());
--- katamari.js
+++ katamari.js
@@ -24,3 +24,4 @@
 	
 	console.log('loading done');
   }
+  }
