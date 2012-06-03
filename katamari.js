26f9e931c91bec2f85a427adba85cad2d98ba434
Ilkka Turunen <ilkka@nestronite.com>
2012-06-03 22:37:36 +0300

    Created skeleton katamari.js
    Created katamari.js with an embedded jquery loader from google's hosting
    site. Loader code graciously stolen from bantic's awesome wordless web.
    Should alert if loading is complete.

---
+++ katamari.js
@@ -0,0 +1,24 @@
+;(function() {
+/*
+* Javascript and idea partially stolen from http://github.com/bantic/wordless-web
+*/
+  // This is hosted off of github
+  // because we don't want to deal with setting up and serving off of SSL
+  // through our domain provider
+  window.__wlw = {};
+  window.__wlw.loading_jQuery = false;
+  window.__wlw.loader = function() {
+    if (typeof(jQuery) === 'undefined') {
+      if (window.__wlw.loading_jQuery) {
+        setTimeout(window.__wlw.loader, 100);
+      } else {
+        window.__wlw.loading_jQuery= true;
+        var jq_script = document.createElement('script');
+        jq_script.src = '//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+        document.body.appendChild(jq_script);
+        window.__wlw.loader();
+      }
+    } else {
+		alert('JQuery present!');
+    }
+  }
