e6376ef8afe6de3153bf1366bd8406be419378f6
Ilkka Turunen <ilkka@nestronite.com>
2012-06-03 23:45:41 +0300

    debugging

--- katamari.js
+++ katamari.js
@@ -42,7 +42,7 @@
 		console.info('jQuery UI present');
 	
 	console.log('testing hiliter');
-	jQuery("div").click(function () {
+	jQuery("div").click(function() {
 	 jQuery(this).effect("highlight", {}, 3000);
 });
 	console.log('hiliter set');
